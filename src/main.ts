9eeee01afcbc8c25d33814438ca1a4c6cc1f1442
Steve Smith <seniorcreative@MacBook-Pro.local>
2019-10-16 22:06:46 +1100

    First commits of vue files, Bulma etc

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
+import './assets/sass/main.scss';
+
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container is-fluid">
+    <div class="notification">
+      This container is <strong>fluid</strong>: it will have a 32px gap on either side, on any
+      viewport size.
+      <img alt="Vue logo" src="../assets/logo.png">
+      <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    </div>
   </div>
 </template>
 
