2adaba8f080f3092e8d5bb1f37048b36b6135e72
Steven Smith <steve@seniorcreative.com.au>
2019-10-17 21:58:26 +1100

    Add cards to landing page

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -7,9 +7,48 @@
         <div class="notification">This is a demo!</div>
 
         <section class="columns">
-          <div class="column">Col 1</div>
-          <div class="column">Col 2</div>
-          <div class="column">Col 3</div>
+          <div class="column">
+            <article class="message">
+              <div class="message-header">
+                <p>Hello World</p>
+                <button class="delete" aria-label="delete"></button>
+              </div>
+              <div class="message-body">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum
+                <a>felis venenatis</a> efficitur. Aenean ac
+                <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+              </div>
+            </article>
+          </div>
+          <div class="column">
+            <article class="message">
+              <div class="message-header">
+                <p>Hello World</p>
+                <button class="delete" aria-label="delete"></button>
+              </div>
+              <div class="message-body">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum
+                <a>felis venenatis</a> efficitur. Aenean ac
+                <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+              </div>
+            </article>
+          </div>
+          <div class="column">
+            <article class="message">
+              <div class="message-header">
+                <p>Hello World</p>
+                <button class="delete" aria-label="delete"></button>
+              </div>
+              <div class="message-body">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum
+                <a>felis venenatis</a> efficitur. Aenean ac
+                <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+              </div>
+            </article>
+          </div>
         </section>
       </div>
     </div>
@@ -17,7 +56,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue } from "vue-property-decorator";
 
 @Component
 export default class Home extends Vue {}
